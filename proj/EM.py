2e845ecc1fb5f21c2a1b4204be3044b47bfc10ac
Arrows <Arrows@59.66.108.116>
2015-10-25 07:23:21 +0800

    Update

--- proj/EM.py
+++ proj/EM.py
@@ -1,6 +1,16 @@
 import scipy
 import numpy
+from numpy import matrix
+from matplotlib.projections.polar import ThetaFormatter
 
-
+class EMAlgorithm:
+    K = 0
+    Ng = 0
+    Ne = []
+    SUMe = 0
+    
+    z = matrix.zeros()
+    
+    
 
 print('test')
