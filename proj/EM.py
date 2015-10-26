e5fca9a0941b2e30c02ec6cd98f0a065e3e2ff9c
Arrows <Arrows@59.66.108.116>
2015-10-26 09:08:10 +0800

    Update

--- proj/EM.py
+++ proj/EM.py
@@ -1,16 +1,43 @@
-import scipy
-import numpy
-from numpy import matrix
-from matplotlib.projections.polar import ThetaFormatter
+import scipy as scp
+import numpy as np
 
 class EMAlgorithm:
-    K = 0
-    Ng = 0
-    Ne = []
-    SUMe = 0
-    
-    z = matrix.zeros()
-    
-    
+    def __init__(self, NG, NE, NW, K):
+        self.NG = NG
+        self.NE = NE
+        self.K = K
+        self.NW = NW
+        
+        self.L = []
+        for g in range(self.NG):
+            self.L.append(np.zeros((1, self.NE[g])))
+        
+        self.C = []
+        for s in range(self.NW):
+            self.C.append([])
+            for g in range(self.NG):
+                self.C[s].append(np.zeros((1, self.NE[g])))
+        
+        self.W = np.zeros((1, self.NW))
+        self.Z = np.zeros((1, self.NG))
+        self.X = []
+        for g in range(self.NG):
+            self.X.append(np.zeros((1, self.NE[g])))
+            
+        self.Mu = np.zeros((self.NW, self.NG))
+        self.A = []
+        for g in range(self.NG):
+            self.A = np.zeros((self.numberOfConstraints(self.NE[g]), self.NE[g]))
 
-print('test')
+        self.initialByKmerTable()
+
+    def numberOfConstraints(self, NEg):
+        return np.round(0.5 *(NEg * NEg + 4 * NEg - 5))
+    
+    def initialByKmerTable(self):
+        pass
+    
+    
+     
+test = EMAlgorithm(3, [2, 3, 5], 57, 25)
+print(test)
