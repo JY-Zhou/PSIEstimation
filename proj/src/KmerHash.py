3dfaccb40bd19e4915192ebd217b24b39ff0168a
Arrows <sherry521007@gmail.com>
2015-10-31 22:26:05 +0800

    Sync up

--- proj/src/KmerHash.py
+++ proj/src/KmerHash.py
@@ -1,9 +1,8 @@
-from src.SolverPipeline import exonBoundaryFile
 class KmerHash:
     def __init__(self, K, genomeFile, exonBoundaryFile, readsFile):
         self.K = K
         self.kmerTable = {}
-        self.gene = []
+        self.geneBoundary = []
         self.readReads(readsFile)
         self.readGenome(genomeFile, exonBoundaryFile)
     
@@ -16,40 +15,37 @@ class KmerHash:
             else:
                 self.kmerTable[read] = 0
         self.NW = len(self.kmerTable)
-        return 
+        return
     
     def readGenome(self, genomeFile, exonBoundaryFile):
         exonBoundaryIn = open(exonBoundaryFile,'r')
         COL_EXONNUM = 1
         COL_EXONST = 2
-        COL_EXONED = 3
-                
+        COL_EXONED = 3                
         for line in exonBoundaryIn:
             sub = line[:-1].split('\t')
-            self.gene.append([])
+            self.geneBoundary.append([])
             subst = sub[COL_EXONST].split(',')
             subed = sub[COL_EXONED].split(',')
             e = 0
             while e < int(sub[COL_EXONNUM]):
-                self.gene[-1].append((int(subst[e]), int(subed[e])))
+                self.geneBoundary[-1].append((int(subst[e]), int(subed[e])))
                 e += 1
+        exonBoundaryIn.close()
         
-        self.NG = len(self.gene)
+        self.NG = len(self.geneBoundary)
         self.NE = []
         for g in range(self.NG):
-            self.NE.append(len(self.gene[g]))
+            self.NE.append(len(self.geneBoundary[g]))
         
-        exonBoundaryIn.close()
-                
         genomeIn = open(genomeFile, 'r')
-
-        #go on ...
-                
-        
-        
+        geneSeq = ''
+        for line in genomeIn:
+            if '>' not in line:
+                geneSeq += line[:-1]
+        genomeIn.close()
+        for g in range(self.NG):
+            
+            ...
+            
         return
-    
-        
-    def work(self):
-        return       
-        
--- proj/src/SolverPipeline.py
+++ proj/src/SolverPipeline.py
@@ -1,11 +1,10 @@
-from KmerHash import KmerHash
-from EMAlgorithm import EMAlgorithm
+from src.KmerHash import KmerHash
+from src.EMAlgorithm import EMAlgorithm
 
 exonBoundaryFile = r'../input/exonBoundary.bed'
 genomeFile = r'../input/genome.fa'
 readsFile = r'../input/reads.fq'
 K = 25
 hasher = KmerHash(K, genomeFile, exonBoundaryFile, readsFile)
-hasher.work()
 solver = EMAlgorithm()
 solver.work()
