c4535641b452a45461e15fafaba715fc842e8408
Arrows <sherry521007@gmail.com>
2015-11-24 22:20:16 +0800

    Sync

--- proj/src/SolverPipeline.py
+++ proj/src/SolverPipeline.py
@@ -1,8 +1,6 @@
 from src.KmerHash import KmerHash
 from src.EMAlgorithm import EMAlgorithm
-
-import matplotlib.pyplot as plt
-import numpy as np
+import json
 
 exonBoundaryFile = r'../input/exonBoundary.bed'
 genomeFile = r'../input/genome.fa'
@@ -19,34 +17,19 @@ for x in kmerHasher.temp:
         print('0')
 solver = EMAlgorithm(kmerHasher)
 solver.work(1)
-#===============================================================================
-# 
-# xax = []
-# y1ax = []
-# y2ax = []
-# y3ax = []
-# vx = np.matrix([[0.4, 0.4, 0.2]])
-# sx2 = solver.Tau.dot(vx.T)
-# print(vx)
-# print(solver.likelihoodFunction(vx))
-# sx3 = solver.Tau.dot(solver.X[0].T)
-# print(solver.X[0])
-# print(solver.likelihoodFunction(solver.X[0]))
-# 
-# i = 0
-# for x in kmerHasher.temp:
-#     xax.append(kmerHasher.id[x])
-#     y1ax.append(kmerHasher.kmerTable[x][0])
-#     #y2ax.append(sx2[i, 0]*1000)
-#     #y3ax.append(sx3[i, 0]*1000)
-#     y2ax.append(y1ax[-1] / sx2[i, 0])
-#     y3ax.append(y1ax[-1] / sx3[i, 0])
-#     #y1ax[-1] *= 1000
-#     i += 1
-#     
-# plt.grid(True)
-# #plt.plot(xax, y1ax, 'b')
-# plt.plot(xax, y2ax, 'r')
-# plt.plot(xax, y3ax, 'g')
-# plt.show()
-#===============================================================================
+
+GX = json.load(open('../kits/XGroundTruth.json', 'r'))
+print(solver.X)
+for g in range(solver.NG):
+    tot = 0.0
+    for i in range(solver.NX[g]):
+        GX[g][i] *= solver.L[g][0,i]
+        tot += GX[g][i]
+    for i in range(solver.NX[g]):
+        GX[g][i] /= tot
+        solver.X[g][0, i] = GX[g][i]
+print(GX)
+print(solver.X)
+print(solver.likelihoodFunction(GX[0]))
+solver.computePSI()
+print(solver.Psi)
